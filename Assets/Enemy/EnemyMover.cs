1f1f93a12f923a7b52faf28ba8ec89426bfd81f7
gpw pc <gabrielbgsantos@hotmail.com>
2023-01-28 15:50:09 -0300

    Enemy moves smoothly / enemy faces the position it is going to

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
+    //[SerializeField] float waitTime = 1f;
 
     void Start()
     {
@@ -15,8 +16,22 @@ public class EnemyMover : MonoBehaviour
     IEnumerator FollowPath () {
         foreach (Waypoint way in path) {
             Debug.Log(way.name);
-            transform.position = way.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = way.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition); // face the end position
+
+            while(travelPercent < 1f) {
+                travelPercent += Time.deltaTime * speed;
+                Debug.Log(travelPercent);
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent); // travelPercent goes from 0 to 1, being 0 the startPos and 1 the endPos
+                yield return new WaitForEndOfFrame(); //wait till the end of the frame (isn't it obvious?)
+            }
+    
+            // transform.position = way.transform.position;
+            // yield return new WaitForSeconds(waitTime);
         }
     }
 }
